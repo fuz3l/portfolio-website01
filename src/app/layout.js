77d6dd94477a596d38c1c8df9992990187ed0fef
fuz3l <connect.fuzailmansuri@gmail.com>
2025-03-04 12:06:46 +0530

    vercel analytics added

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-
+import { Analytics } from '@vercel/analytics/next';
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
@@ -83,6 +83,7 @@ export default function RootLayout({ children }) {
         <Header />
         <main className="flex-grow container mx-auto px-4 py-8">{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   )
